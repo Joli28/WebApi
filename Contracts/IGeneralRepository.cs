fdb8fb3b8fbe996a7da62d5f6f6182f170d49d48
Arjoli <49071418+Joli28@users.noreply.github.com>
2020-09-20 19:21:27 +0200

    Add files via upload

---
+++ Contracts/IGeneralRepository.cs
@@ -0,0 +1,22 @@
+﻿using Entities.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contracts
+{
+    public interface IGeneralRepository
+    {
+        bool isSetConnectionString();
+        void SetConnectionString(string connectionString);
+        List<string> GetDeget();
+        List<DitaDto> GetDitet();
+        List<KlasaDto> GetKlasa();
+        List<string> GetParaleli(string dega, int viti);
+        List<string> GetPedagog();
+        List<OrariDateKlaseDto> GetOrariDateKlaseDtos(int klasa, int dita);
+        List<int> GetVitetPerDege(string dega);
+        List<OrariStudentDto> GetOrariStudent(string dega, int viti, string paraleli);
+        List<OrarPedagogDto> GetOrarPedagog(string emri);
+    }
+}
